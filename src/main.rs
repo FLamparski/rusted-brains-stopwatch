bf4b8e558ae4fbbd5312d0edc1365049f9db7da1
Filip Wieland <filiplamparski@gmail.com>
2019-10-19 22:28:09 +0100

    Implement full blogpost spec

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ extern crate stm32f4xx_hal as hal;
 
 use crate::hal::{
     prelude::*,
+    gpio::{ExtiPin, Edge, PullUp, Input, gpioc::PC13},
     rcc::{Rcc, Clocks},
     i2c::I2c,
     stm32,
@@ -17,22 +18,33 @@ use crate::hal::{
     delay::Delay,
     interrupt,
 };
-use cortex_m::interrupt::{Mutex, free};
+use cortex_m::interrupt::{Mutex, free, CriticalSection};
 use cortex_m_rt::{entry};
-use cortex_m_semihosting::hprintln;
-use embedded_graphics::{fonts::Font6x12, prelude::*};
+use embedded_graphics::{fonts::{Font6x12, Font12x16}, prelude::*};
 use ssd1306::{prelude::*, Builder as SSD1306Builder};
 use core::cell::{Cell, RefCell};
-use core::ops::DerefMut;
+use core::ops::{DerefMut};
 use core::fmt;
 use arrayvec::ArrayString;
 
 static ELAPSED_MS: Mutex<Cell<u32>> = Mutex::new(Cell::new(0u32));
 static TIMER_TIM2: Mutex<RefCell<Option<Timer<stm32::TIM2>>>> = Mutex::new(RefCell::new(None));
+static STATE: Mutex<Cell<StopwatchState>> = Mutex::new(Cell::new(StopwatchState::Ready));
+static EXTI: Mutex<RefCell<Option<stm32::EXTI>>> = Mutex::new(RefCell::new(None));
+static BUTTON: Mutex<RefCell<Option<PC13<Input<PullUp>>>>> = Mutex::new(RefCell::new(None));
+
+#[derive(Clone, Copy)]
+enum StopwatchState {
+    Ready,
+    Running,
+    Stopped
+}
 
 #[entry]
 fn main() -> ! {
-    if let (Some(dp), Some(cp)) = (stm32::Peripherals::take(), cortex_m::Peripherals::take()) {
+    if let (Some(mut dp), Some(cp)) = (stm32::Peripherals::take(), cortex_m::Peripherals::take()) {
+        dp.RCC.apb2enr.write(|w| w.syscfgen().enabled());
+
         let rcc = dp.RCC.constrain();
         let clocks = setup_clocks(rcc);
         let gpiob = dp.GPIOB.split();
@@ -45,8 +57,12 @@ fn main() -> ! {
             400.khz(),
             clocks,
         );
+
         let gpioc = dp.GPIOC.split();
-        let _board_btn = gpioc.pc13.into_pull_up_input();
+        let mut board_btn = gpioc.pc13.into_pull_up_input();
+        board_btn.make_interrupt_source(&mut dp.SYSCFG);
+        board_btn.enable_interrupt(&mut dp.EXTI);
+        board_btn.trigger_on_edge(&mut dp.EXTI, Edge::FALLING);
 
         let mut disp: GraphicsMode<_> = SSD1306Builder::new().connect_i2c(i2c).into();
         disp.init().unwrap();
@@ -54,12 +70,21 @@ fn main() -> ! {
 
         let mut timer = Timer::tim2(dp.TIM2, 1.khz(), clocks);
         timer.listen(Event::TimeOut);
-        free(|cs| *TIMER_TIM2.borrow(cs).borrow_mut() = Some(timer));
+
+        let exti = dp.EXTI;
+        free(|cs| {
+            TIMER_TIM2.borrow(cs).replace(Some(timer));
+            EXTI.borrow(cs).replace(Some(exti));
+            BUTTON.borrow(cs).replace(Some(board_btn));
+        });
+
+        stm32::NVIC::unpend(hal::interrupt::TIM2);
+        stm32::NVIC::unpend(hal::interrupt::EXTI15_10);
+        unsafe {
+            stm32::NVIC::unmask(hal::interrupt::EXTI15_10);
+        };
 
         let mut delay = Delay::new(cp.SYST, clocks);
-
-        stm32::NVIC::unpend(stm32f4xx_hal::interrupt::TIM2);
-        unsafe { stm32::NVIC::unmask(stm32f4xx_hal::interrupt::TIM2); };
 
         loop {
             let elapsed = free(|cs| ELAPSED_MS.borrow(cs).get());
@@ -67,11 +92,24 @@ fn main() -> ! {
             let mut format_buf = ArrayString::<[u8; 10]>::new();
             format_elapsed(&mut format_buf, elapsed);
 
-            let text = Font6x12::render_str(format_buf.as_str())
+            disp.clear();
+
+            let state = free(|cs| STATE.borrow(cs).get());
+            let state_msg = match state {
+                StopwatchState::Ready => "Ready",
+                StopwatchState::Running => "",
+                StopwatchState::Stopped => "Stopped"
+            };
+            let state_text = Font6x12::render_str(state_msg)
                 .with_stroke(Some(1u8.into()))
                 .translate(Coord::new(0, 0));
-            disp.clear();
-            disp.draw(text.into_iter());
+            disp.draw(state_text.into_iter());
+
+            let elapsed_text = Font12x16::render_str(format_buf.as_str())
+                .with_stroke(Some(1u8.into()))
+                .translate(Coord::new(0, 14));
+            disp.draw(elapsed_text.into_iter());
+
             disp.flush().unwrap();
 
             delay.delay_ms(100u32);
@@ -84,7 +122,6 @@ fn main() -> ! {
 #[interrupt]
 fn TIM2() {
     free(|cs| {
-        stm32::NVIC::unpend(stm32f4xx_hal::interrupt::TIM2);
         if let Some(ref mut tim2) = TIMER_TIM2.borrow(cs).borrow_mut().deref_mut() {
             tim2.clear_interrupt(Event::TimeOut);
         }
@@ -92,9 +129,34 @@ fn TIM2() {
         let cell = ELAPSED_MS.borrow(cs);
         let val = cell.get();
         cell.replace(val + 1);
+    });
+}
 
-        // unsafe { (*stm32f405::TIM2::ptr()).sr.modify(|_, w| w.uif().clear_bit()) };
-        // unsafe { (*stm32::TIM2::ptr()).sr.modify(|_, w| w.uif().clear_bit()); };
+#[interrupt]
+fn EXTI15_10() {
+    free(|cs| {
+        let mut btn_ref = BUTTON.borrow(cs).borrow_mut();
+        let mut exti_ref = EXTI.borrow(cs).borrow_mut();
+        if let (Some(ref mut btn), Some(ref mut exti)) = (btn_ref.deref_mut(), exti_ref.deref_mut()) {
+            // We cheat and don't bother checking _which_ exact interrupt line fired - there's only
+            // ever going to be one in this example.
+            btn.clear_interrupt_pending_bit(exti);
+
+            let state = STATE.borrow(cs).get();
+            // Run the state machine in an ISR - probably not something you want to do in most
+            // cases but this one only starts and stops TIM2 interrupts
+            match state {
+                StopwatchState::Ready => {
+                    stopwatch_start(cs);
+                    STATE.borrow(cs).replace(StopwatchState::Running);
+                },
+                StopwatchState::Running => {
+                    stopwatch_stop(cs);
+                    STATE.borrow(cs).replace(StopwatchState::Stopped);
+                },
+                StopwatchState::Stopped => {},
+            }
+        }
     });
 }
 
@@ -106,6 +168,15 @@ fn setup_clocks(rcc: Rcc) -> Clocks {
         .pclk1(24.mhz())
         .pclk2(24.mhz())
         .freeze();
+}
+
+fn stopwatch_start<'cs>(cs: &'cs CriticalSection) {
+    ELAPSED_MS.borrow(cs).replace(0);
+    unsafe { stm32::NVIC::unmask(hal::interrupt::TIM2); }
+}
+
+fn stopwatch_stop<'cs>(_cs: &'cs CriticalSection) {
+    stm32::NVIC::mask(hal::interrupt::TIM2);
 }
 
 fn format_elapsed(buf: &mut ArrayString<[u8; 10]>, elapsed: u32) {
